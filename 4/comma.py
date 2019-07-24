42e20afabcd0ec5731eff4738baa033ebd0e9af0
Austin <you@example.com>
2019-07-23 20:47:33 -0500

    Puesdo based on ch3 homework -in notebook

---
+++ 4/comma.py
@@ -0,0 +1,16 @@
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return (number * 3) + 1
+try:
+    item = input("Enter a number")
+    print(int(number))
+    new_number = collatz(int(number))
+    print(new_number)
+    while new_number != 1:
+        new_number = collatz(new_number)
+        print(new_number)
+except ValueError:
+    print("Bad input")
+

e91d2b02f716020f4b386b5858740f839a17d8d8
Austin <you@example.com>
2019-07-25 19:52:50 -0500

    final comma code

--- 4/comma.py
+++ 4/comma.py
@@ -1,19 +1,29 @@
-def collatz(number):
-    if number % 2 == 0:
-        return number // 2
+def sentence(list):
+    total = len(list) 
+    final = ''
+    count = 0
+    while count < total:
+        if count < (total - 1):
+            final = final + "'" + list[count] + "', "
+            count = count + 1
+        else:
+            final = final +  "'" + list[count] + "'"
+            count = count + 1
+    print(final)
 
-number = input("Enter a number")
+
+number = input("Enter an item for the list ")
 list = [number]
 print(list)
 status = True
 while status == True:
-    number2 = input("Enter a number")
+    number2 = input("Enter an item for the list  ")
     list.append(number2)
-    user_input = input("Do you want to add another number (Y/N)?")
-    print(user_input)
+    user_input = input("Do you want to add another item (Y/N)? ")
     if user_input == 'Y':
         counter = True
     else:
         counter = False
         break
-print(list)
+print("You provided the following list: " + str(list))
+sentence(list)
